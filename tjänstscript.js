f6400712df39f9011d863cd24a0fff0137953041
Lena <lenaappelberg6@gmail.com>
2024-12-19 15:39:33 +0100

    tjänstscriptjs

--- tjänstscript.js
+++ tjänstscript.js
@@ -1,10 +1,30 @@
-const modalbutton= getelementById('toastbutton');
+const modalbutton=document.getElementById('toastbutton');
 const toast=document.querySelector('.toast');
-const closebutton= getelementById('closetoastbutton')
-
+const closebutton=document.getElementById('closetoastbutton')
+const toasttext=document.querySelector('.toasttext')
+const inputformula=document.querySelector(".consult-form");
 modalbutton.onclick= function(){
     toast.style.display="block"
-}
+};
 closebutton.onclick=function(){
     toast.style.display="none"
+};
+inputformula.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const formtime=document.getElementById('time');
+    if (Validtime(formtime.value)) {
+        const textnode=document.createTextNode("Din konsultation är klockan"+ formtime.value);
+        toasttext.appendChild(textnode);
+    } else {
+        throw('error');
+    }
+});
+function validTime(timeinput) {
+    const time=timeinput.split(':');
+    if (0<time[1]<24 && 0<time[2]<60) {
+        return true;
+    } else {
+        return false;
+    }
 }
+module.exports={validTime};
