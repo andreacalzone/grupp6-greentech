4e7f26c72eca13ccc72649d07fabc3ebe5ee9524
Lena <lenaappelberg6@gmail.com>
2024-12-19 16:49:32 +0100

    extratest

--- timeFunc.test.js
+++ timeFunc.test.js
@@ -4,3 +4,8 @@ describe('validTime',()=>{
         expect(validTime("abc")).toBe(false);
     })
 })
+describe('validTime',()=>{
+    it('throw error when kontaktformula receives a number',function(){
+        expect(validTime("a:b")).toBe(false);
+    })
+})
